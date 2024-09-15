4c26a4659b4aa528a8cd90042dafe349976887fa
ujjwalnp <dhakalujjwal99@gmail.com>
2024-09-16 00:11:30 +0545

    feat: update .gitignore

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='intrusion_detection_system',
+    name='IntrusionVigor',
     version='0.1',
     packages=find_packages('src'),
     package_dir={'': 'src'},
